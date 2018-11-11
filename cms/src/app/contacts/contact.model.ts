750551785b8c14da6ac9c01fc96e9d54cb86eab7
Zac Bell <zchrbell7@gmail.com>
2018-11-10 17:08:56 -0700

    Validate the fields in the Contact Edit form

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -5,11 +5,11 @@ export class Contact {
     public id: string;
     public name: string;
     public email: string;
-    public phone: string;
-    public imageUrl: string;
+    public phone?: string;
+    public imageUrl?: string;
     public group?: Contact[];
 
-    constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group?: Contact[]) {
+    constructor(id: string, name: string, email: string, phone?: string, imageUrl?: string, group?: Contact[]) {
         this.id = id;
         this.name = name;
         this.email = email;
