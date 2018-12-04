12d9bb534238797ccfeecfbd3cf8fe36c9a3deb7
Zac Bell <zchrbell7@gmail.com>
2018-12-03 18:04:44 -0700

    ...

--- cms/server/routes/documents.js
+++ cms/server/routes/documents.js
@@ -1,22 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/document/:id').get((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issue);
-    });
+getDocuments((req, res) => {
+  Document.find(req.params.id, (err, document) => {
+    if (err)
+      res.status(500).json({
+        document: 'Error'
+      });
+    else
+      res.status(200).json({
+        document: 'Added successfully'
+      });
+  });
 });
 
-router.route('/document/add').post((req, res) => {
-    let issue = new Issue(req.body);
-    issue.save()
-        .then(issue => {
-            res.status(200).json({'document': 'Added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new document');
-        });
-});
+// router.route('/document/:id').get((req, res) => {
+//     Document.findById(req.params.id, (err, document) => {
+//         if (err)
+//             console.log(err);
+//         else
+//             res.json(issue);
+//     });
+// });
+
+// router.route('/document/add').post((req, res) => {
+//     let issue = new Issue(req.body);
+//     issue.save()
+//         .then(issue => {
+//             res.status(200).json({'document': 'Added successfully'});
+//         })
+//         .catch(err => {
+//             res.status(400).send('Failed to create new document');
+//         });
+// });
